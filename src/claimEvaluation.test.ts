294cfee5098f8e99bfc33cd722dd72951ee6f813
Joe Rouse <loodwigx@gmail.com>
2025-04-29 04:18:31 -0600

    Fleshing out validation tests

--- src/claimEvaluation.test.ts
+++ src/claimEvaluation.test.ts
@@ -3,18 +3,56 @@ import { performEvaluation, valiadteClaimFormat } from "./claimEvaluation.ts";
 
 describe("Validate Claim Format", () => {
   test("Legitimate Claim", () => {
+    const legit = {
+      policyId: "POL123",
+      incidentType: "fire",
+      incidentDate: new Date("2023-06-15"),
+      amountClaimed: 3000
+    }
+    expect(valiadteClaimFormat(legit)).toBe(true);
   });
 
   test("Bogus id", () => {
+    const bogus = {
+      policyId: 123,
+      incidentType: "fire",
+      incidentDate: new Date("2023-06-15"),
+      amountClaimed: 3000
+    }
+    expect(valiadteClaimFormat(bogus)).toBe(false);
   });
 
   test("Bogus type", () => {
+    const bogus = {
+      policyId: "POL123",
+      incidentType: "tornado",
+      incidentDate: new Date("2023-06-15"),
+      amountClaimed: 3000
+    }
+    expect(valiadteClaimFormat(bogus)).toBe(false);
   });
 
   test("Bogus date", () => {
+    const bogus = {
+      policyId: "POL123",
+      incidentType: "fire",
+      incidentDate: 0,
+      amountClaimed: 3000
+    }
+    expect(valiadteClaimFormat(bogus)).toBe(false);
   });
 
   test("Bogus amount", () => {
+    const bogus = {
+      policyId: "POL123",
+      incidentType: "fire",
+      incidentDate: 0,
+      amountClaimed: "A decent amount"
+    }
+    expect(valiadteClaimFormat(bogus)).toBe(false);
+  });
+});
+
 describe("Perform Evaluation", () => {
   test("Perfect Claim", () => {
     const evaluation = performEvaluation({
